e05b28464cacbe63327a94e03e8cc10dc2d5b008
KaiHofstetter <kai.hofstetter@gmx.de>
2015-02-28 07:27:36 +0100

    wp-cli is not using root anymore.

--- config_wordpress.sh
+++ config_wordpress.sh
@@ -2,7 +2,9 @@
 
 if ! $(wp core is-installed  --allow-root --path='/var/www/html'); then
    echo "=> WordPress is not configured yet, configuring WordPress ..."
-   wp core install --path='/var/www/html' --allow-root --url='localhost' --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL"
+
+   echo "=> installing WordPress"
+   sudo -E -u www-data wp core install --path='/var/www/html' --url='localhost' --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL"
 else
    echo "=> WordPress is alread configured.";
 fi
